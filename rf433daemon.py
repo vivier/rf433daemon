10b7d7d27a22741015d583c8a5c9bb8b99d04e8a
Laurent Vivier <laurent@vivier.eu>
2019-07-17 16:49:35 +0200

    add python version of the daemon
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

---
+++ rf433daemon.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import configparser
+import serial
+import io
+import logging
+
+config = configparser.ConfigParser()
+config.read('eray.conf')
+config.sections()
+
+def dump_config():
+    for key in config['devices']:
+        print(key + " " + config['devices'][key])
+
+    for key in config['commands']:
+        print(key + " " + config['commands'][key])
+
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+logger = logging.getLogger("ERAY")
+logger.setLevel(logging.INFO)
+
+ch = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+while True:
+    value = int(ser.readline(), 16)
+    device = value >> 8
+    command = value & 0xff
+
+    try:
+        device_name = config['devices'][format(device, '04X')]
+    except:
+        device_name = "(Unknown device)"
+
+    try:
+        command_name = config['commands'][format(command, '02X')]
+    except:
+        command_name = "(Unknown command)"
+
+    logger.info(format(value, '06X') + ' ' + device_name + ': ' + command_name)
